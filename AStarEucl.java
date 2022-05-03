eabb66b0ac9b1be987608b19ba6db4c6394fd3c6
rfujita1 <60188873+rfujita1@users.noreply.github.com>
2022-05-03 14:20:31 -0400

    quick changes

--- AStarEucl.java
+++ AStarEucl.java
@@ -21,7 +21,6 @@ public class AStarEucl {
         Node curr = graph.getEnd();
         while (curr.getCameFrom() != null) {
             curr.setPath(true);
-           // System.out.println(curr.getX()+","+curr.getY());
             curr = curr.getCameFrom();
         }
         graph.getStart().setPath(true);
@@ -36,7 +35,6 @@ public class AStarEucl {
 
             Node low = open.get(0);
             for (Node n : open) {
-                //System.out.print(n.getX()+","+n.getY()+": ");
                 if (n.getF() < low.getF()) {
                     low = n;
                 }
@@ -46,8 +44,6 @@ public class AStarEucl {
 
             closed.add(curr);
             open.remove(curr);
-            //if(curr!=graph.getStart())
-                //System.out.println(curr.getF()+": "+curr.getCameFrom().getX()+curr.getCameFrom().getY());
             ArrayList<Node> neighbors = curr.getNeighbors();
             for (int i = 0; i < neighbors.size(); i++) {
                 Node neighbor = neighbors.get(i);
@@ -65,26 +61,18 @@ public class AStarEucl {
                     } else {
                         neighbor.setG(tempG);
                         open.add(neighbor);
-                        //System.out.println("Quagmire");
                         neighbor.setH((double) heristic(neighbor, graph.getEnd()));
                         neighbor.setF(neighbor.getG() + neighbor.getH());
                         neighbor.setCameFrom(curr);
                     }
-                    //
-                    
-                    //System.out.println(curr.getX()+","+curr.getY()+": "+neighbor.getX()+","+neighbor.getY()+":: "+neighbor.getF());
-                    
+                                        
                 }
-                //System.out.println(curr.getX()+","+curr.getY()+": "+i);
             }
-            // graph.printGraph();
-            // System.out.println("--------------------------------------");
             if (low == graph.getEnd()) {
                 findPath();
                 System.out.println("Found!");
                 return 0;
             }
-            //System.out.println("-----------");
         }
 
         System.out.println("Not Found :(");
