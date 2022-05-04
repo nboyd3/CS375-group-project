329d20d25df6f17a1800d7923539010bf0dea403
rfujita1 <rfujita1@binghamton.edu>
2022-05-03 23:28:19 -0400

    diag Changes

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@ public class Main {
 
     public static void main(String args[]) {
         
-        Graph eucledianGrid = new Graph(50, 0, 0, 49, 49, "Eucledian");
+        Graph diagonalGrid = new Graph(50, 0, 0, 49, 49, "Diagonal");
         Graph manhattanGrid = new Graph(50, 0, 0, 49, 49, "Manhattan");
 
         // creates new Astar object using grid
         Astar astar = new Astar(manhattanGrid);
-        AStarEucl eucl = new AStarEucl(eucledianGrid);
+        AStarDiagonal eucl = new AStarDiagonal(diagonalGrid);
 
         // times the algorithm
         double startTime = System.nanoTime();
