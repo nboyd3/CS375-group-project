a264c7c0a5b9220a622bc49e02ecd635f5610e38
rfujita1 <60188873+rfujita1@users.noreply.github.com>
2022-05-04 00:25:58 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -12,19 +12,20 @@ public class Main {
             // creates new Astar object using grid
             Astar astar = new Astar(manhattanGrid);
             AStarDiagonal eucl = new AStarDiagonal(diagonalGrid);
-
+            System.out.println("Manhattan:");
             // times the algorithm
             double startTime = System.nanoTime();
             astar.run();
             double endTime = System.nanoTime();
             double duration = (endTime - startTime);
             //System.out.format("%.2fms\n", duration / 1_000_000);
-            
+            System.out.println("Diagonal:");
             startTime = System.nanoTime();
             eucl.run();
             endTime = System.nanoTime();
             duration = (endTime - startTime);
             //System.out.format("%.2fms\n", duration / 1_000_000);
+            System.out.println("------------------------------------------------------");
             }
     }
 }
