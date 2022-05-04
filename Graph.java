43017e5cbdf2132f9b211e32b27add29faba8196
rfujita1 <rfujita1@binghamton.edu>
2022-05-04 00:23:32 -0400

    all obstacles

--- Graph.java
+++ Graph.java
@@ -6,16 +6,18 @@ public class Graph {
     private int ex;
     private int ey;
     private String type;
+    private int environment;
 
     private Node graph[][];
 
-    public Graph(int deminsion, int sx, int sy, int ex, int ey, String type) {
+    public Graph(int deminsion, int sx, int sy, int ex, int ey, String type, int environment) {
         this.deminsion = deminsion; // graph deminsion (square)
         this.sx = sx; // start x
         this.sy = sy; // start y
         this.ex = ex; // end x
         this.ey = ey; // end y
         this.type=type;
+        this.environment=environment;
         graph = new Node[deminsion][deminsion]; // 2d Node array
 
         // fill graph with Nodes
@@ -38,28 +40,59 @@ public class Graph {
         // graph[3][4].setObstacle(true);
 
         /*Environment: Manhattan doesn't work but euclidean does*/
-        // for(int i=0; i<10; i++)
-        //         graph[i][9-i].setObstacle(true);
+        if(environment==0){
+            if(type=="Diagonal")
+            System.out.println("Environment: Manhattan doesn't work but euclidean does");
+            for(int i=0; i<deminsion; i++)
+                graph[i][deminsion-1-i].setObstacle(true);
+        }
+        
         // /*Environment: Only affects euclidean*/
-        //     graph[4][4].setObstacle(true);
-        //     graph[4][3].setObstacle(true);
-        //     graph[3][4].setObstacle(true);
+        if(environment==1){
+            if(type=="Diagonal")
+            System.out.println("Environment: Only affects euclidean");
+            graph[4][4].setObstacle(true);
+            graph[4][3].setObstacle(true);
+            graph[3][4].setObstacle(true);
+        }    
         // /*Environment: Only affects manhattan*/
-        //     for(int i=2; i<10; i++)
-        //         graph[1][i].setObstacle(true);
+        if(environment==2){
+            if(type=="Diagonal")
+            System.out.println("Environment: Only affects manhattan");
+            for(int i=2; i<deminsion; i++)
+                graph[1][i].setObstacle(true);
+        }
+      
         
         /*Environment: Euclean runtme should be much longer*/
-        for(int i=1; i<50; i++)
-            for(int j=0; j<49; j++)
-                graph[i][j].setObstacle(true);
-        for(int i=0; i<48; i++)
-            graph[i][i].setObstacle(false);
+        if(environment==3){
+            if(type=="Diagonal")
+            System.out.println("Environment: Euclean runtme should be much longer");
+            for(int i=1; i<deminsion; i++)
+                for(int j=0; j<deminsion-1; j++)
+                    graph[i][j].setObstacle(true);
+            for(int i=0; i<deminsion-2; i++)
+                graph[i][i].setObstacle(false);
+        }
+        
         /*Environment: random obstacles*/
-        //  for(int i=0; i<50-1; i++){
-        // //     int rand1 = (int)(Math.random() * 50 );
-        // //     int rand2 = (int)(Math.random() * 50 );
-        //      graph[35][i].setObstacle(true);
-        //  }
+        if(environment==4){
+            if(type=="Diagonal")
+            System.out.println("Environment: random obstacles*");
+            for(int i=0; i<10; i++){
+                int rand1 = (int)(Math.random() * deminsion );
+                int rand2 = (int)(Math.random() * deminsion );
+                graph[rand1][rand2].setObstacle(true);
+            }
+        }
+         
+        if(environment==5){
+            if(type=="Diagonal")
+            System.out.println("Environment: middle barrier");
+            for(int i=0; i<deminsion-1; i++){
+                graph[deminsion/2][i].setObstacle(true);
+            }
+        }
         
            
         // get neighbors for all nodes
